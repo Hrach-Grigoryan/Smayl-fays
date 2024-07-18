0aeb3e43cf0046fadec7be0a89ea28bffa45b568
Hrach-Grigoryan <grigoryanhrach97@gmail.com>
2024-07-18 13:14:14 +0300

    smayl

---
+++ js/script.js
@@ -0,0 +1,51 @@
+'use strict'
+//----------------------chi stacvel
+// var btn = document.getElementById(' btn1')
+// btn.addEventListener('click', function(){
+//     var btn1 = document.getElementById('htn')
+//     btn1.style.height = '150px'
+//     btn1.style.width = '150px'
+
+// })
+
+//-------------------   Beran   -------------------\\
+function myfunction(){
+    var btn1 = document.getElementById('htn')
+    btn1.style.width = '150px' 
+    btn1.style.height = '7px'
+
+}   
+  
+function myfunction1(){
+    var btn1 = document.getElementById('htn')
+    btn1.style.width = '150px' 
+    btn1.style.height = '7ex' 
+}   
+
+//-----------------     eye 1  ----------------\\ 
+
+function myfunction3(){
+    var btn2 = document.getElementById('eye1')
+    btn2.style.width = '1px' 
+    btn2.style.height = '7px    ' 
+}
+
+function myfunction4(){
+    var btn2 = document.getElementById('eye1')
+    btn2.style.width = '30px' 
+    btn2.style.height = '30px' 
+}
+
+//-----------------     eye 2  ----------------\\ 
+
+function myfunction5(){
+    var btn2 = document.getElementById('eye2')
+    btn2.style.width = '1px' 
+    btn2.style.height = '7px    ' 
+}
+
+function myfunction6(){
+    var btn2 = document.getElementById('eye2')
+    btn2.style.width = '30px' 
+    btn2.style.height = '30px' 
+}
